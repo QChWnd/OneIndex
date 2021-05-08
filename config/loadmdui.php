ba403289354a56afa48a2b6f53a60f9bb52f6842
QChWnd <60031666+QChWnd@users.noreply.github.com>
2021-05-08 17:15:44 +0800

    Delete loadmdui.php

--- config/loadmdui.php
+++
@@ -1,4 +0,0 @@
-<?php return array (
-    'loadmdui_css' => '<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/mdui@1.0.1/dist/css/mdui.min.css" integrity="sha384-cLRrMq39HOZdvE0j6yBojO4+1PrHfB7a9l5qLcmRm/fiWXYY+CndJPmyu5FV/9Tw" crossorigin="anonymous"/>',
-    'loadmdui_js' => '<script src="https://cdn.jsdelivr.net/npm/mdui@1.0.1/dist/js/mdui.min.js" integrity="sha384-gCMZcshYKOGRX9r6wbDrvF+TcCCswSHFucUzUPwka+Gr+uHgjlYvkABr95TCOz3A" crossorigin="anonymous"></script>',
-);
