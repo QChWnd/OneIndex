e10f255710d94715dcab466b10ae5aecd5feddd0
QChWnd <60031666+QChWnd@users.noreply.github.com>
2021-05-08 17:44:38 +0800

    Update video.php

--- view/nexmoe/show/video.php
+++ view/nexmoe/show/video.php
@@ -31,5 +31,5 @@ const dp = new DPlayer({
 	}
 });
 </script>
-<a href="<?php e($url);?>" class="mdui-fab mdui-fab-fixed mdui-ripple mdui-color-theme-accent"><i class="mdui-icon material-icons">file_download</i></a>
+<a href="<?php e($url);?>" class="mdui-fab mdui-fab-fixed mdui-ripple mdui-color-theme-<?php e(config('nexmoe_mdui-color-theme-accent_color'));?>"><i class="mdui-icon material-icons">file_download</i></a>
 <?php view::end('content');?>
