f5c1f8b0a32dc04ae3701623f5409286d2dade85
QChWnd <60031666+QChWnd@users.noreply.github.com>
2021-05-08 21:26:50 +0800

    Update list.php

--- view/nexmoe/list.php
+++ view/nexmoe/list.php
@@ -173,7 +173,7 @@ function thumb(){
 				var href = $(this).parent().parent().attr('href');
 				var thumb =(href.indexOf('?') == -1)?'?t=220':'&t=220';
 				$(this).hide();
-				$(this).parent().parent().parent().css("background","url("+href+thumb+")  no-repeat center top");
+				$(this).parent().parent().parent().css('background', 'url('+href+thumb+') center top / cover no-repeat');
 			}
 		});
 	}
